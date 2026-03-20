68ab9dfefdb96bd2234a49b488243dcd38fe3518
Enno Richter <enno@nerdworks.de>
2026-03-20 09:46:44 +0100

    feat: Add Homebrew formula for opendataloader-pdf

---
+++ Formula/opendataloader-pdf.rb
@@ -0,0 +1,27 @@
+class OpendataloaderPdf < Formula
+  desc "PDF parser for AI-ready data extraction (Markdown, JSON, HTML)"
+  homepage "https://github.com/opendataloader-project/opendataloader-pdf"
+  url "https://github.com/opendataloader-project/opendataloader-pdf/releases/download/v2.0.2/opendataloader-pdf-cli-2.0.2.zip"
+  sha256 "06793fbff8824c1eb452972a0c5d8b729d5c019afda5db7149809fdae8b68638"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "opendataloader-pdf-cli-#{version}.jar" => "opendataloader-pdf-cli.jar"
+
+    (bin/"opendataloader-pdf").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/opendataloader-pdf-cli.jar" "$@"
+    EOS
+  end
+
+  test do
+    assert_match "format", shell_output("#{bin}/opendataloader-pdf 2>&1")
+  end
+end
